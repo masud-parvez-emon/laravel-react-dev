0b717630171cadd7de7671825f68b920c7274c93
masud-parvez-emon <masud.parvez.emon@gmail.com>
2025-12-07 03:22:31 +0600

    app integrate with mui-tailwind

--- resources/js/src/main.tsx
+++ resources/js/src/main.tsx
@@ -6,6 +6,10 @@ import ReactDOM from 'react-dom/client'
 
 // Tailwind css
 import './tailwind.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 // i18n (needs to be bundled)
 import './i18n';
@@ -18,15 +22,19 @@ import router from './router/index';
 import { Provider } from 'react-redux';
 import store from './store/index';
 import Router from './router/index';
+import { GlobalStyles, StyledEngineProvider } from '@mui/material';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <Suspense>
-            <Provider store={store}>
-                <Router/>
-            </Provider>
-        </Suspense>
+        <StyledEngineProvider enableCssLayer>
+            <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+            <Suspense>
+                <Provider store={store}>
+                    <Router/>
+                </Provider>
+            </Suspense>
+        </StyledEngineProvider>
     </React.StrictMode>
 );
 
